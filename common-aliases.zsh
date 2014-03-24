dcd26781e7c5a5f3538d1c73fb04a47457fb745f
Gary Iams <ge.iams@gmail.com>
2014-03-24 15:30:13 -0400

    Add common aliases for ls, readlink, etc.

---
+++ common-aliases.zsh
@@ -0,0 +1,12 @@
+alias i='itunes'
+
+alias ls='ls -GF'
+alias ll='ls -l'
+alias la='ll -a'
+alias l='ll | $PAGER'
+alias lt='ll -t'
+
+alias ql=quick-look
+alias rl='readlink'
+
+alias ts='du -h -d0 $HOME/.Trash'
